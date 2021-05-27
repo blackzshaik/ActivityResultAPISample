f3cfb3bdd8ce7dad565200a6a5a6c1a2eae764e0
blackzshaik <blackzshaik@gmail.com>
2021-05-27 22:32:46 +0530

    request permission initial commit

--- app/src/main/java/com/madtutorial/activityresult/HomeFragment.kt
+++ app/src/main/java/com/madtutorial/activityresult/HomeFragment.kt
@@ -1,26 +1,44 @@
 package com.madtutorial.activityresult
 
-import android.content.Intent
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 
 
 class HomeFragment : Fragment() {
 
-    private val secondActivityWithResult =
-        registerForActivityResult(CustomContractParcelable<SampleModel>()) { result ->
-            if (result != null) {
-                resultTextView.text = result.description
+    private val requestStoragePermission =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissionGranted ->
+
+            if (permissionGranted.isNotEmpty()) {
+                if (permissionGranted[permissionArray[0]]!! && permissionGranted[permissionArray[1]]!!) {
+                    permissionStatusTextView.text = "Permission Granted"
+                } else {
+                    permissionStatusTextView.text = "User denied permission"
+                }
             }
         }
 
-    private lateinit var getResultButton: Button
-    private lateinit var resultTextView: TextView
+
+    private lateinit var requestPermissionButton: Button
+    private lateinit var permissionStatusTextView: TextView
+
+    private val permissionArray = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,14 +48,52 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        getResultButton = view.findViewById(R.id.getResultButton)
-        resultTextView = view.findViewById(R.id.resultTextView)
+        requestPermissionButton = view.findViewById(R.id.requestPermissionButton)
+        permissionStatusTextView = view.findViewById(R.id.permissionStatusTextView)
 
-        getResultButton.setOnClickListener {
-            secondActivityWithResult.launch(Intent(requireContext(), SecondActivity::class.java))
+
+        requestPermissionButton.setOnClickListener {
+            checkStoragePermission()
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun checkStoragePermission() {
+
+        if (isAllPermissionGranted()) {
+            permissionStatusTextView.text = "Permission Already Granted"
+        } else {
+            if (requireActivity().shouldShowRequestPermissionRationale(permissionArray[0])
+                || requireActivity().shouldShowRequestPermissionRationale(permissionArray[1])
+            ) {
+                Toast.makeText(
+                    requireContext(),
+                    "In order to demonstrate the success scenario we need you to allow access to the permission",
+                    Toast.LENGTH_LONG
+                ).show()
+
+                requestStoragePermission.launch(permissionArray)
+            } else {
+                requestStoragePermission.launch(permissionArray)
+            }
+        }
+
+    }
+    
+
+    private fun isAllPermissionGranted(): Boolean {
+        permissionArray.forEach {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    it
+                ) == PackageManager.PERMISSION_DENIED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
 }
